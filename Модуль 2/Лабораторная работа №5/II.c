b4bc2ff3d0f9ea5a4e8e6ebc95d07499d554ba60
Никита Пырлицану <nikitapyrlicanu@MacBook-Pro-Nikita.local>
2023-11-14 12:05:22 +0300

    Transfer

--- Модуль 2/Лабораторная работа №5/II.c
+++ Модуль 2/Лабораторная работа №5/II.c
@@ -2,33 +2,32 @@
 #include <stdlib.h>
 #include "../../safeIO.h"
 #include <string.h>
+#include <locale.h>
 
 #define STRING_SIZE 50
 #define FILE_NAME_SIZE 10
 // Variant(6, 6) 
 
 int isrus(char sym) {
-    printf("%d %c\n", sym, sym);
     return sym<0;
 }
 
-void delstrind(char str[STRING_SIZE], int ind) {
-    for (int i=ind; i<STRING_SIZE-2; i++)
-        str[ind] = str[ind+2];
+void delstrind(char *str, int ind) {
+    for (int i=ind; i<STRING_SIZE-1; i++)
+        str[i] = str[i+1];
 }
 
 void delrus(char str[STRING_SIZE]) {
     for (int i=0; i<STRING_SIZE && str[i] != '\0'; i++) {
-        printf("%s\n", str);
         if (isrus(str[i])) {
             delstrind(str, i);
-            printf("%d\n", i);
             if (i+1 != STRING_SIZE) i--;
         }
     }
 }
 
 int main() {
+    setlocale(LC_ALL, "Russian");
     char name[FILE_NAME_SIZE];
     printf("Enter input file name:\n");
     enterString(name, FILE_NAME_SIZE);
@@ -51,7 +50,7 @@ int main() {
         if (k != -1) {
             delrus(str);
             printf("%s\n", str);
-            //fprintf(fo, "%s\n", str);
+            fprintf(fo, "%s\n", str);
         }
     } while (k != -1);
 
